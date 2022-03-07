808a0e97728d921a78dde0840ad942aab10c9d60
animaliads-team <ds.animalia@gmail.com>
2022-03-07 15:26:21 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ani-icon
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ani-icon)
---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import '@animaliads/css-tokens/global-tokens.css';
+import '@animaliads/animalia-brand/theme.css';
+import '@animaliads/ani-icon';
+import './style.css';
+
+export default function App() {
+  return <ani-icon name="arrow-left"></ani-icon>;
+}
